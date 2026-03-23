868fb194e55ff4dae55a7cef4c3e6eefdc67df4f
XSpace-WolfX <corentinbeck@outlook.fr>
2026-03-23 09:45:19 +0200

    Move tools-drawer from shared/components to shared/ui directory
    - Relocate tools-drawer component files to shared/ui
    - Update import path in app-shell.ts

--- src/app/layout/app-shell/app-shell.ts
+++ src/app/layout/app-shell/app-shell.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ToolsDrawerComponent } from '../../shared/components/tools-drawer/tools-drawer';
+import { ToolsDrawerComponent } from '../../shared/ui/tools-drawer/tools-drawer';
 
 @Component({
   selector: 'ccm-app-shell',
